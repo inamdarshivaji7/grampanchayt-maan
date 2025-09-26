9e28d3e2b35ad76a481e110df303516314ca2925
inamdarshivaji7 <inamdarshivaji7@gmail.com>
2025-09-26 13:54:04 +0530

    created new project and designed home page gp maan

---
+++ gramapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Member(models.Model):
+    name = models.CharField(max_length=100)
+    designation = models.TextField()
+    contact = models.IntegerField()
+    photo = models.ImageField(upload_to='post_images/', blank=True, null=True)
+
+
+    def __str__(self):
+        return self.name
