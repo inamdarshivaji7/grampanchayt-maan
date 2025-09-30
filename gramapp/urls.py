5fdc0a31f25cb2daf2486ca982dbe770d0a9657e
inamdarshivaji7 <inamdarshivaji7@gmail.com>
2025-09-30 17:33:43 +0530

    added images and started working on members page

--- gramapp/urls.py
+++ gramapp/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
 from . import views
+from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),  # root of this app
+    path('members/', views.member_list, name='member_list'),
 ]
--- gramapp/views.py
+++ gramapp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Member
 
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
     return render(request, 'gramapp/index.html')
+
+def member_list(request):
+    members = Member.objects.all()[3:]
+
+    return render(request, 'gramapp/members/member_list.html', {'members': members})
