b72eb4761dbf0cca955975f26de058338a9a14a8
ashish <ashishdede05@gmail.com>
2025-10-10 11:46:26 +0530

    added employee profle and member profile page

--- gramapp/urls.py
+++ gramapp/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),  # root of this app
     path('members/', views.member_list, name='member_list'),
     path('employees/', views.employee_list, name='employee_list'),
+    path('employee/<int:employee_id>/', views.employee_profile, name='employee_profile'),
+    path('members/<int:member_id>/', views.member_profile, name='member_profile'),
+
 ]
--- gramapp/views.py
+++ gramapp/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Member,Employee
 
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'gramapp/index.html')
+    top_members = Member.objects.all()[:3]
+    return render(request, 'gramapp/index.html',{'top_members': top_members})
 
 def member_list(request):
     members = Member.objects.all()[3:]
@@ -16,3 +17,12 @@ def employee_list(request):
     employees = Employee.objects.all()
 
     return render(request, 'gramapp/employee/employee_list.html', {'employees': employees})
+def employee_profile(request, employee_id):
+    
+    employee = get_object_or_404(Employee, id=employee_id)
+    return render(request, 'gramapp/employee/employee_profile.html', {'employee': employee})
+
+def member_profile(request, member_id):
+    
+    member = get_object_or_404(Member, id=member_id)
+    return render(request, 'gramapp/members/member_profile.html', {'member': member})
